c8deafcfd764a9a09d3972fdb2afa2803c828c0f
jstiven01 <stiven10@gmail.com>
2020-03-10 11:00:41 -0500

    Creating initial app

---
+++ src/components/MainProduct.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const MainProduct = () => {
+    return (
+        <div className="main">
+            <h1>For Gamers by Gamers Tee</h1>
+         </div> 
+         );
+}
+ 
+export default MainProduct;
