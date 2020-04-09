cab5ace2c58f23c6cdeb6a63efe303b4f03da5fb
jstiven01 <stiven10@gmail.com>
2020-04-09 09:47:51 -0500

    Updates

---
+++ precache-manifest.cf40c78364f974776a1802e159dccd29.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841bd7af434551fc811882425da8cd4d",
+    "url": "/games-clothing/index.html"
+  },
+  {
+    "revision": "f2b82a76be85e40a4480",
+    "url": "/games-clothing/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "d7ebb9b74effda0120c9",
+    "url": "/games-clothing/static/css/main.359730d0.chunk.css"
+  },
+  {
+    "revision": "f2b82a76be85e40a4480",
+    "url": "/games-clothing/static/js/2.ff6168de.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/games-clothing/static/js/2.ff6168de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ebb9b74effda0120c9",
+    "url": "/games-clothing/static/js/main.a397bfb4.chunk.js"
+  },
+  {
+    "revision": "38e522d199cc4a4718d9",
+    "url": "/games-clothing/static/js/runtime-main.2e2e7e00.js"
+  },
+  {
+    "revision": "2d6b26b831e442e3434ce603cc4047b1",
+    "url": "/games-clothing/static/media/mainPhoto2.2d6b26b8.png"
+  },
+  {
+    "revision": "244052d2ee1fa6b9010c05faa805236b",
+    "url": "/games-clothing/static/media/product1.244052d2.png"
+  },
+  {
+    "revision": "9fcd52fe47588143cbccee6055477a29",
+    "url": "/games-clothing/static/media/product2.9fcd52fe.png"
+  },
+  {
+    "revision": "ee5c5772a150bdf95b7c8250778e578a",
+    "url": "/games-clothing/static/media/product3.ee5c5772.png"
+  },
+  {
+    "revision": "3a56efb8cac7a931f8bd9afd9d078f1a",
+    "url": "/games-clothing/static/media/product4.3a56efb8.png"
+  },
+  {
+    "revision": "a5e5794273744da91fc47407d3fad402",
+    "url": "/games-clothing/static/media/product5.a5e57942.png"
+  },
+  {
+    "revision": "7dd045dcd80394804faad4ab75c8f83c",
+    "url": "/games-clothing/static/media/product6.7dd045dc.png"
+  }
+]);
